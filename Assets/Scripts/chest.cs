2abd3865ba491a28af364e25fb176143658ed2f2
BeatriceGhigi <beaghigi44@gmail.com>
2026-03-19 10:59:23 +0100

    apertura chest

---
+++ Assets/Scripts/chest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour
+{
+   private Animator animator;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator= this.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("open", true);
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+   
+}
